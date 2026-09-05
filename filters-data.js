dfadc197b7decea81360d9bcf394f90d672c761f
rayjmorrow <ray@hottubfactoryoutlet.com>
2026-09-05 10:51:32 -0400

    Add starter exact filter catalog data

---
+++ filters-data.js
@@ -0,0 +1,7 @@
+window.HTFO_FILTERS=[
+{id:'OC-508B',brand:'MAAX Spas',shopBrands:['American Whirlpool','Vita Spa'],oncore:'OC-508B',unicel:'5CH-35',pleatco:'PMAX50P4',od:'5',length:'8',top:'Open',bottom:'Open',insideDiameter:'',price:null},
+{id:'OC-508BM',brand:'MAAX Spas',shopBrands:['American Whirlpool','Vita Spa'],oncore:'OC-508BM',unicel:'5CH-35AM',pleatco:'PMAX50P4-M',od:'5',length:'8',top:'Open',bottom:'Open',insideDiameter:'',price:null},
+{id:'OC-508C',brand:'MAAX Spas',shopBrands:['American Whirlpool','Vita Spa'],oncore:'OC-508C',unicel:'',pleatco:'PMAX25P4',od:'5',length:'8',top:'Open',bottom:'Open',insideDiameter:'',price:null},
+{id:'OC-508D',brand:'MAAX Spas',shopBrands:['American Whirlpool','Vita Spa'],oncore:'OC-508D',unicel:'',pleatco:'PMAX50P3',od:'5',length:'8',top:'Open',bottom:'Open',insideDiameter:'',price:null}
+];
+// Starter catalog only. Exact dimensions/end configurations will be expanded from manufacturer catalog data before online ordering is enabled.
